fe8802d20351770a9d659b09b9b12521d5d33e44
tatipar <tsparlanti@gmail.com>
2022-02-24 16:07:40 -0300

    first graph generated from pandas df
    read csv file and store it as dataframe (capture20110810 only)
    check if there is at least one reverse
    create dataframes SrcAdds->DstAddr and DstAddr->SrcAddr
    concatenate dataframes and add the weights if there are repeated links
    create a graph and calculate degree and IDW,ODW

--- code/python/issue12.py
+++ code/python/issue12.py
@@ -19,19 +19,29 @@ for i in range(len(c20110810["SrcAddr"])):
     if control:
         break
             
-
+#armo dataframes con conexiones SrcAddr->DstAddr y las respuestas DstAddr->SrcAddr
 ida=c20110810[["SrcAddr","DstAddr","SrcBytes"]].copy()
 vuelta=c20110810[["DstAddr","SrcAddr","DstBytes"]].copy()
 ida.rename(columns={'SrcAddr': 'origen', 'DstAddr': 'destino', 'SrcBytes': 'peso'}, inplace=True)
 vuelta.rename(columns={'DstAddr': 'origen', 'SrcAddr': 'destino', 'DstBytes': 'peso'}, inplace=True)
 
+#junto los dataframes anteriores y sumo los pesos (bytes) en caso que haya varias conexiones entre los mismos nodos
 tutti=pd.concat([ida, vuelta], ignore_index=True)
 df=tutti.groupby(['origen','destino'], as_index=False)['peso'].sum().copy()
 
-vertices=pd.DataFrame(list(set(df["origen"])))
 
+#armo grafo
 g=Graph.DataFrame(df, directed=True)
-g.degree()
-g.degree(mode='in')
-g.degree(mode='out')
+
+#pruebas uso del grafo:
+
+#calculo de grados
+grados=g.degree()
+gradosIn=g.degree(mode='in')
+gradosOut=g.degree(mode='out')
 sum(i>150 for i in grados)
+
+#calculo de IDW, ODW
+vertices=pd.DataFrame(list(set(df["origen"]))) #tiene TODOS los vertices porque "origen" tiene SrcAddr y DestAddr
+g.strength(vertices[0].tolist(), mode='all', weights=df["peso"])
+
