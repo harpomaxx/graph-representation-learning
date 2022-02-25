d0a54118df17bf6bf9fcd4a44c4f94ed27bf9c5d
tatipar <tsparlanti@gmail.com>
2022-02-25 17:18:37 -0300

    add ways to generate the graph

--- code/python/issue12.py
+++ code/python/issue12.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import igraph
 from igraph import Graph
-import sys
+#import sys
 
-#pruebo con el primer archivo
+#first capture (20110810)
 
+#read csv file and store it as dataframe
 c20110810 = pd.read_csv("capture20110810_forGraph.csv")
-#c20110810[["SrcAddr","DstAddr","SrcBytes","DstBytes"]] #podria cambiar el orden de las columnas
 
-#controla si existe al menos una reversa 
+#c20110810[["SrcAddr","DstAddr","SrcBytes","DstBytes"]] #to change the order of columns
+
+#check if there is at least one reverse
 control = False
 for i in range(len(c20110810["SrcAddr"])):
     for j in range(len(c20110810["DstAddr"])):
@@ -19,29 +21,42 @@ for i in range(len(c20110810["SrcAddr"])):
     if control:
         break
             
-#armo dataframes con conexiones SrcAddr->DstAddr y las respuestas DstAddr->SrcAddr
+#create dataframes with conections SrcAddr->DstAddr and responses DstAddr->SrcAddr
 ida=c20110810[["SrcAddr","DstAddr","SrcBytes"]].copy()
 vuelta=c20110810[["DstAddr","SrcAddr","DstBytes"]].copy()
 ida.rename(columns={'SrcAddr': 'origen', 'DstAddr': 'destino', 'SrcBytes': 'peso'}, inplace=True)
 vuelta.rename(columns={'DstAddr': 'origen', 'SrcAddr': 'destino', 'DstBytes': 'peso'}, inplace=True)
 
-#junto los dataframes anteriores y sumo los pesos (bytes) en caso que haya varias conexiones entre los mismos nodos
+#concatenate dataframes and add the weights if there are repeated links
 tutti=pd.concat([ida, vuelta], ignore_index=True)
 df=tutti.groupby(['origen','destino'], as_index=False)['peso'].sum().copy()
 
+##################################################################################
+#create an NCOL file to then generate the graph  
+df.to_csv('cap20110810.ncol',sep=' ',header=None, index=None)
+#at another time:
+grafo=Graph.Read_Ncol('cap20110810.ncol',weights=True,directed=True)
 
-#armo grafo
+##################################################################################
+
+#create a graph at the moment
 g=Graph.DataFrame(df, directed=True)
 
-#pruebas uso del grafo:
 
-#calculo de grados
+#calculate degree
 grados=g.degree()
 gradosIn=g.degree(mode='in')
 gradosOut=g.degree(mode='out')
 sum(i>150 for i in grados)
 
-#calculo de IDW, ODW
+#calculate degree weight
 vertices=pd.DataFrame(list(set(df["origen"]))) #tiene TODOS los vertices porque "origen" tiene SrcAddr y DestAddr
 g.strength(vertices[0].tolist(), mode='all', weights=df["peso"])
 
+
+
+#ANOTHER WAY TO CREATE THE GRAPH
+G=Graph.TupleList(df.values,weights=True,directed=True)
+G.strength(G.vs,mode='all',weights=G.es['weight']) # G.vs['name'].sort()==list(vertices).sort() 
+                                                   # G.es['weight'].sort()==list(df["peso"]).sort()
+G.write_ncol("c20110810.ncol") # to load later
