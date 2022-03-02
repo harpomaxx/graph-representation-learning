38f32f83cecfc6737520d1290113cd6fd4f3d1b6
tatipar <tsparlanti@gmail.com>
2022-03-02 17:32:56 -0300

    extract SrcAddr DstAddr TotBytes SrcBytes (#12)
    For each capture:
    unzip file
    extract SrcAddr DstAddr TotBytes SrcBytes
    subtract TotBytes-SrcBytes (to obtain DstBytes)
    create a .csv file and update header
    store .csv in /rawdata/ctu-13/ip_bytes/csvFiles/

---
+++ code/bash/001_extract_ip_and_bytes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Note: to run in /rawdata/ctu-13 (directory that has compressed files)
+
+
+mkdir -p ip_bytes/csvFiles
+
+for filename in ./*.gz; do 
+    gunzip $filename    # unzip
+    grep "\"" ${filename%.*}    # check that there are no quotation marks (because there could be commas inside quotation marks)
+
+    if [ $? -ne 0 ]; then
+        awk -F "," '{print $4, $7, $13, $14}' ${filename%.*} > ${filename%.*.*.*}_tmp   # extract SrcAddr DstAddr TotBytes SrcBytes
+        awk -v OFS="," '$3-=$4' ${filename%.*.*.*}_tmp | sed '1iSrcAddr,DstAddr,DstBytes,SrcBytes'> ${filename%.*.*.*}_ip_bytes.csv   # subtract TotBytes-SrcBytes (to obtain DstBytes)
+                                                                                                                                      # create a .csv file
+                                                                                                                                      # update header 
+    else
+        echo "Please use another method because there may be commas inside quotation marks" > ${filename%.*.*.*}_warning.txt
+    fi
+done    
+
+mv *_ip_bytes.csv ip_bytes/csvFiles/
+rm -r *_tmp
+
+for filename in ./*.labels; do 
+    zip -q ${filename#./*}.gz ${filename#./*}
+done    
+
+rm -r *.labels
+
