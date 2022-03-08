b185fabbb3c8742aabbd4dbdf8835788a3a3b71e
tatipar <tsparlanti@gmail.com>
2022-03-08 13:10:35 -0300

    add filter on protocol (#12)
    store SrcAddr DstAddr DstBytes SrcBytes in csv files
    (only for connections with protocol "tcp" or "udp")

--- code/bash/001_extract_ip_and_bytes.sh
+++ code/bash/001_extract_ip_and_bytes.sh
@@ -10,7 +10,7 @@ for filename in ./*.gz; do
     grep "\"" ${filename%.*}    # check that there are no quotation marks (because there could be commas inside quotation marks)
 
     if [ $? -ne 0 ]; then
-        awk -F "," '{print $4, $7, $13, $14}' ${filename%.*} > ${filename%.*.*.*}_tmp   # extract SrcAddr DstAddr TotBytes SrcBytes
+        awk -F "," '{if ($3=="tcp" || $3=="udp") print $4, $7, $13, $14}' ${filename%.*} > ${filename%.*.*.*}_tmp   # extract SrcAddr DstAddr TotBytes SrcBytes
         awk -v OFS="," '$3-=$4' ${filename%.*.*.*}_tmp | sed '1iSrcAddr,DstAddr,DstBytes,SrcBytes'> ${filename%.*.*.*}_ip_bytes.csv   # subtract TotBytes-SrcBytes (to obtain DstBytes)
                                                                                                                                       # create a .csv file
                                                                                                                                       # update header 
