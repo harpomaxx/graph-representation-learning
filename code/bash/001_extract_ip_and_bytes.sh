d398a7418f5fafde72935a9e523ca23b84ad7232
tatipar <tsparlanti@gmail.com>
2022-03-11 18:31:08 -0300

    correct TotBytes-SrcBytes
    correct awk rule to calculate TotBytes-SrcBytes for all rows
    modify directory names
    use path of the top-level directory for reference

--- code/bash/001_extract_ip_and_bytes.sh
+++ code/bash/001_extract_ip_and_bytes.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# Note: to run in /rawdata/ctu-13 (directory that has compressed files)
+PATH_REPO=`git rev-parse --show-toplevel`   # show the path of the top-level directory
 
+cd ${PATH_REPO}/rawdata/ctu-13/    # Note: to run in /rawdata/ctu-13 (directory that has compressed files)
 
-mkdir -p ip_bytes/csvFiles
+mkdir -p ip_bytes/csv_sIP-dIP-dB-sB
 
 for filename in ./*.gz; do 
     gunzip $filename    # unzip
     grep "\"" ${filename%.*}    # check that there are no quotation marks (because there could be commas inside quotation marks)
 
     if [ $? -ne 0 ]; then
-        awk -F "," '{if ($3=="tcp" || $3=="udp") print $4, $7, $13, $14}' ${filename%.*} > ${filename%.*.*.*}_tmp   # extract SrcAddr DstAddr TotBytes SrcBytes
-        awk -v OFS="," '$3-=$4' ${filename%.*.*.*}_tmp | sed '1iSrcAddr,DstAddr,DstBytes,SrcBytes'> ${filename%.*.*.*}_ip_bytes.csv   # subtract TotBytes-SrcBytes (to obtain DstBytes)
-                                                                                                                                      # create a .csv file
-                                                                                                                                      # update header 
+        awk -F "," '{if ($3=="tcp" || $3=="udp") print $4, $7, $13, $14}' ${filename%.*} > ${filename%.*.*.*}_tmp   # filter protocols "tcp" and "udp"
+                                                                                                                    # extract SrcAddr DstAddr TotBytes SrcBytes
+                                                                                                                    
+        awk -v OFS="," '{$3-=$4}{print $0}' ${filename%.*.*.*}_tmp | sed '1iSrcAddr,DstAddr,DstBytes,SrcBytes' > ${filename%.*.*.*}_ip_bytes.csv   # subtract TotBytes-SrcBytes (to obtain DstBytes)
+                                                                                                                                                   # add header 
+                                                                                                                                                   # create a .csv file
     else
         echo "Please use another method because there may be commas inside quotation marks" > ${filename%.*.*.*}_warning.txt
     fi
 done    
 
-mv *_ip_bytes.csv ip_bytes/csvFiles/
+mv *_ip_bytes.csv ip_bytes/csv_sIP-dIP-dB-sB/
 rm -r *_tmp
 
 for filename in ./*.labels; do 
--- code/bash/002_create_ncol.sh
+++ code/bash/002_create_ncol.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Note: to run in /rawdata/ctu-13/ip_bytes/csvFiles
+PATH_REPO=`git rev-parse --show-toplevel`   # show the path of the top-level directory
 
+cd ${PATH_REPO}/rawdata/ctu-13/ip_bytes/csv_sIP-dIP-dB-sB/  # Note: to run in /rawdata/ctu-13/ip_bytes/csv_sIP-dIP-dB-sB
 
-mkdir ../ncolFiles
+mkdir ../ncol_sIP-dIP-B
 
 for filename in ./*.csv; do 
     base=`basename ${filename} .csv`
-    python3 ../../../../code/python/002_create_ncol.py ${base%_*_*} 
+    python3 ${PATH_REPO}/code/python/002_create_ncol.py ${base%_*_*} 
 done    
 
-mv *.ncol ../ncolFiles/
+mv *.ncol ../ncol_sIP-dIP-B/
 
 
