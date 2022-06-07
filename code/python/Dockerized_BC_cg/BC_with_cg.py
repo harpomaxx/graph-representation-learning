38207a86edf1aa4ab1ed7c3caa6504efe3df7778
EduPav <edumarpav@yahoo.com.ar>
2022-06-07 15:56:49 -0300

    Docker built for cugraph

--- code/python/Dockerized_BC_cg/BC_with_cg.py
+++ code/python/Dockerized_BC_cg/BC_with_cg.py
@@ -6,7 +6,7 @@ import json
 ctuName="capture20110815-2.binetflow.labels.gz"
 
 # read data into a cuDF DataFrame using read_csv
-gdf = cudf.read_csv('../../data/csv/'+ctuName, names=["SrcAddr", "DstAddr","Bytes"], dtype=["str", "str","float32"])
+gdf = cudf.read_csv('./'+ctuName+".csv", names=["SrcAddr", "DstAddr","Bytes"], dtype=["str", "str","float32"])
 
 # We now have data as edge pairs
 
