1f3e9e977ed2597c3d98f41a6401993ccf61b2ff
tatipar <tsparlanti@gmail.com>
2023-06-21 15:30:06 -0300

    agregar nan cuando corresponde graficar

--- code/python/metricas_txt2csv_and_join.py
+++ code/python/metricas_txt2csv_and_join.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
 import sys
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 PATH_PRINC = str(sys.argv[1]) # path de interes, por ejemplo: "/home/tati/Nextcloud/BotChase/graph-representation-learning/rawdata/synthetic/resultados/Dir_100nodes_balanced_clasesSep/NO_flattened"
 CUAL = str(sys.argv[2]) # cual archivo: "test", "val" o "train"
@@ -23,7 +26,7 @@ for i in range(10):
     auc = []
 
     texto = open(os.path.join(PATH_PRINC, f'prueba_0{i}/metricas/metricas_{CUAL}.txt'), "r")
-
+    
     for line in texto:
         depurar = [quitar(x) for x in line.split() if quitar(x)!='' and ")" not in quitar(x) and "=" not in quitar(x)]
         numeros = [y for y in depurar if y.replace('.','').isdigit()]   
@@ -35,28 +38,49 @@ for i in range(10):
                 FP.append(eval(numeros[0]))
                 TN.append(eval(numeros[1]))
         elif depurar!=[] and depurar[0] == "accuracy":
-            accuracy.append(eval(numeros[0]))
+            if depurar[1] == "nan":
+                accuracy.append(np.nan)
+            else:
+                accuracy.append(eval(numeros[0]))
         elif depurar!=[] and depurar[0] == "precision":
-            precision.append(eval(numeros[0]))
+            if depurar[1] == "nan":
+                precision.append(np.nan)
+            else:
+                precision.append(eval(numeros[0]))
         elif depurar!=[] and depurar[0] == "recall":
-            recall.append(eval(numeros[0]))
+            if depurar[1] == "nan":
+                recall.append(np.nan)
+            else:
+                recall.append(eval(numeros[0]))
         elif depurar!=[] and depurar[0] == "especificity":
-            especificity.append(eval(numeros[0]))
+            if depurar[1] == "nan":
+                especificity.append(np.nan)
+            else:
+                especificity.append(eval(numeros[0]))
         elif depurar!=[] and depurar[0] == "f1":
-            f1.append(eval(numeros[0]))
+            if depurar[1] == "nan":
+                f1.append(np.nan)
+            else:
+                f1.append(eval(numeros[0]))
         elif depurar!=[] and depurar[0] == "auc_score":
-            auc.append(eval(numeros[0]))
+            if depurar[1] == "nan":
+                auc.append(np.nan)
+            else:
+                auc.append(eval(numeros[0]))
         else:
             pass
-        
+           
     texto.close()
-
+    
     df = pd.DataFrame({"TP":TP, "FN":FN, "FP":FP, "TN":TN, "accuracy":accuracy, "precision":precision, "recall":recall, "especificity":especificity, "f1":f1, "auc_score":auc})
-
+    
     df.to_csv(os.path.join(PATH_PRINC, f'prueba_0{i}/metricas/metricas_{CUAL}.csv'),index = None)
       
 
 ######################################################################################
+
+joinDirectorio = os.path.join(PATH_PRINC, "join")
+os.makedirs(joinDirectorio, exist_ok = True)
 
 df1 = pd.read_csv(os.path.join(PATH_PRINC, f'prueba_00/metricas/metricas_{CUAL}.csv'))
 
@@ -65,6 +89,16 @@ for i in range(1,10):
     p = pd.read_csv(ARCHIVO)
     df1 = pd.concat([df1, p], ignore_index=True)
     
-df1.to_csv(os.path.join(PATH_PRINC, f'join_{CUAL}.csv'),index = None)
+df1.to_csv(os.path.join(joinDirectorio, f'join_{CUAL}.csv'),index = None)
 
-
+    
+#sns.set_theme(style="whitegrid")
+boxplot1 = sns.boxplot(data=df1.iloc[:,7:8]) 
+plt.savefig(os.path.join(joinDirectorio,f'especificity_{CUAL}.png'))                             
+plt.clf()
+    
+#sns.set_theme(style="whitegrid")
+boxplot2 = sns.boxplot(data=df1.iloc[:,9:10]) 
+plt.savefig(os.path.join(joinDirectorio,f'auc_{CUAL}.png'))                             
+plt.clf()
+    
